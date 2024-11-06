fec7e70f7c864530acd87ec7f58ba17e957f27e1
Marina Parra <marinaparrag8@uma.es>
2024-11-06 20:24:04 +0100

    Fallo main2 corregido

--- ChallengeFabrica/src/main/java/Main2.java
+++ ChallengeFabrica/src/main/java/Main2.java
@@ -5,23 +5,27 @@ public class Main2 {
 
         Fabrica fabrica = new Fabrica();
 
-        fabrica.addMaquina(new Fresadora(Grosor.Medio, OrFresa.Horizontal, Posicion.CeSu));
-        fabrica.addMaquina(new Taladradora(Posicion.CeSu,Grosor.Medio));
         fabrica.addMaquina(new Fresadora(Grosor.Medio, OrFresa.Vertical, Posicion.IzCe));
-        fabrica.addMaquina(new Fresadora(Grosor.Medio, OrFresa.Diagonal, Posicion.IzIn));
+        fabrica.addMaquina(new Fresadora(Grosor.Medio, OrFresa.Vertical, Posicion.IzIn));
+        fabrica.addMaquina(new Fresadora(Grosor.Grueso, OrFresa.Diagonal, Posicion.IzIn));
         fabrica.addMaquina(new Taladradora(Posicion.IzCe,Grosor.Medio));
-        fabrica.addMaquina(new Taladradora(Posicion.IzIn,Grosor.Medio));
+        fabrica.addMaquina(new Taladradora(Posicion.IzIn,Grosor.Grueso));
         fabrica.addMaquina(new Lijadora(Posicion.CeCe, OrLija.Norte, Grosor.Medio));
         fabrica.addMaquina(new Rotadora(Sentido.Antihorario));
         fabrica.addMaquina(new Rotadora(Sentido.Antihorario));
         fabrica.addMaquina(new Lijadora(Posicion.IzCe, OrLija.Norte, Grosor.Medio));
         fabrica.addMaquina(new Rotadora(Sentido.Horario));
         fabrica.addMaquina(new Lijadora(Posicion.CeCe, OrLija.Norte, Grosor.Grueso));
-        fabrica.addMaquina(new Taladradora(Posicion.IzIn,Grosor.Grueso));
+        fabrica.addMaquina(new Taladradora(Posicion.CeCe,Grosor.Grueso));
+        fabrica.addMaquina(new Fresadora(Grosor.Medio, OrFresa.Vertical, Posicion.IzCe));
+        fabrica.addMaquina(new Taladradora(Posicion.IzCe,Grosor.Fino));
         
 
         Pieza pieza = new Pieza();
 
+        for (Maquina maquina : fabrica.getMaquinas()) {
+            maquina.actua(pieza);
+        }
 
         System.out.println("Representación textual de la pieza:");
         System.out.println(pieza);
