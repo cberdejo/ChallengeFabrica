1e45927a86eaa0e2aed3cbfdedbe02c8d51454b1
Marina Parra <marinaparrag8@uma.es>
2024-11-06 09:57:21 +0100

    Cambios Lijadora y main2

--- ChallengeFabrica/src/main/java/fabrica/Lijadora.java
+++ ChallengeFabrica/src/main/java/fabrica/Lijadora.java
@@ -1,89 +1,102 @@
 package fabrica;
 
+import java.util.*;
+
 public class Lijadora implements Maquina {
 
     private Grosor grosor;
     private OrLija orLija;
-    private Posicion pos;
+    private Posicion posicion;
 
-    static Set<Posicion> posicionesValidas = EmunSet.of(
+    static Set<Posicion> posicionesValidas = EnumSet.of(
             Posicion.IzSu,
             Posicion.IzCe,
             Posicion.IzIn,
             Posicion.CeSu,
-            Posicion.CeCe,
-
+            Posicion.CeCe
     );
 
-    public Fresadora(Grosor grosor, OrLija orLija, Posicion pos) {
-
-        if(!posicionesValidas.contains(posicion)){
-            throw new IllegalArgumentException("Posición no válida: "+pos)
+    public Lijadora(Posicion posicion,OrLija or,Grosor grosor) {
+        if (!posicionesValidas.contains(posicion)) {
+            throw new IllegalArgumentException("Posición inválida: " + posicion);
         }
-        this.grosor = grosor;
-        this.orLija = orLija;
-        this.pos = pos;
     }
 
     @Override
     public void actua(Pieza pieza) {
-        /// Obtener el cuadro en la posicion de la pieza
+        // Obtener el cuadro en la posición de la pieza
         Cuadro cuadro = pieza.getCuadro(posicion);
 
-        /// Lista de las taladradoras en el cuadro
-        List<String> taladradoras = cuadro.getTaladradoras();
+        // Determinar el grosor de la lijadora como un valor entero
+        int grosorLijadora;
+        switch (this.grosor) {
+            case Grueso:
+                grosorLijadora = 3;
+                break;
+            case Medio:
+                grosorLijadora = 2;
+                break;
+            case Fino:
+            default:
+                grosorLijadora = 1;
+                break;
+        }
 
-        ///  Comprobar si hay taladradoras en la lista
-        for(String taladradora : taladradoras){
-            if(taladradora.matches("TL[1-3]")){
-                /// Extraer el grosor de la taladradora
-                int grosorTaladradora = Integer.paserInt(taladradora.substring(2));
-                /// Comparacion del grosor
-                if(grosorToValue(grosor)>grosorTaladradora){
-                    /// Eliminar el taladro si el grosor es menor que el de la lija
-                    eliminarTaladradora(cuadro, taladradora)
+        // Comprobar si la lija que se va a colocar es de grosor Fino
+        if (this.grosor == Grosor.Fino) {
+            // Obtener el estado actual del taladro
+            String taladroActual = cuadro.getTaladradora();
+
+            // Comprobar el grosor del taladro actual
+            int grosorTaladroActual = 0;
+            if (taladroActual.matches("TL([1-3])")) {
+                grosorTaladroActual = Integer.parseInt(taladroActual.substring(2)); // Obtener el grosor del taladro
+            }
+
+            // Verificar si hay un taladro grueso
+            if (grosorTaladroActual == 3) {
+                // Si hay un taladro grueso, la lija fina no se coloca
+                return;
+            }
+        }
+
+        // Obtener el conjunto de lijas en el cuadro
+        Set<String> lijas = new HashSet<>(cuadro.getLijas());
+
+
+        for (String lijaActual : lijas) {
+            // Comprobar si ya hay una lija en la posición deseada
+            String orientacionExistente = lijaActual.substring(1, 2);
+            int grosorExistente = Integer.parseInt(lijaActual.substring(2));
+
+            // Comprobar si la orientación de la nueva lija es la misma que la existente
+            if (orientacionExistente.equals(getOrientacion())) {
+                // Si el grosor de la nueva lija es mayor que el existente, reemplazarla
+                if (grosorLijadora > grosorExistente) {
+                    cuadro.setLija("L" + getOrientacion() + grosorLijadora);
+                    System.out.println("Lija colocada o reemplazada: " + "L" + getOrientacion() + grosorLijadora);
                 }
             }
         }
 
-        /// Obtener la lija
-        String lijaActual = cuadro.getLija(posicion);
-
-        if(lijaActual.isBlanck() || lijaActual.matches("L[NOSE][123]")){
-            /// Coger el valor del grosor de la lija que hay
-            int grosorExistente = lijaActual.matches("L[NSOE][1-3]")
-                    ? Integer.parseInt(lijaActual.substring(2))
-                    : 0;
-            int grosorLijadora = grosorToValue(grosor);
-            /// Comparar el grosor y comprobar la orientacion
-            if(grosorLijadora> grosor Existente && orLija.equals(lijaActual.charAt(1))){
-                /// si el grosor de la nueva lija es mayor y tienen la misma orientacion, se actualiza la lija en la posicion
-                cuadro.setLija(posicion, "L" + lijaActual.charAt(1) + grosorLijadora);
-            }else {
-                /// Si no, se añade una nueva lija
-                cuadro.addLija(pos, "L" + orLija + grosorLijadora);
-            }
-        }
+        // Si no se encontró ninguna lija con la misma orientación, añadir la nueva lija
+        cuadro.addLija("L" + getOrientacion() + grosorLijadora);
     }
-    /// Convertir el grosor a un valor entero
-    int grosorToValue(Grosor grosor) {
-        switch (grosor) {
-            case Grueso:
-                grosor = 3;
-                break;
-            case Medio:
-                grosor = 2;
-                break;
-            case Fino:
+
+
+    private String getOrientacion() {
+        switch (orLija) {
+            case Norte:
+                return "N";
+            case Sur:
+                return "S";
+            case Este:
+                return "E";
+            case Oeste:
+                return "O";
             default:
-                grosor = 1;
-                break;
+                return "Orientacion no valida";
         }
     }
-
-    /// Eliminar la taladradora
-    private void eliminarTaladradora(Cuadro cuadro, String taladradora) {
-        cuadro.removerTaladradora(taladradora);
-    }
-
 }
+
