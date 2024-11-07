524e59615f4df759716b04569659380ab2115c19
Marina Parra <marinaparrag8@uma.es>
2024-11-07 17:01:02 +0100

    Clase particula documentada

--- ChallengeUniverso/ProyectoUniversoV1/src/main/java/universo/Particula.java
+++ ChallengeUniverso/ProyectoUniversoV1/src/main/java/universo/Particula.java
@@ -8,6 +8,13 @@ public class Particula{
 
     private static final double G = 6.67E-11;
 
+    /** Constructor de particula
+     *
+     * @param pos Posicion inicial de la particula
+     * @param vel La velocidad inicial de la particula
+     * @param m Masa de la particula
+     * @param r Radio dela particula
+     */
     public Particula (Punto pos, Vector vel, double m, double r){
         posicion=pos;
         velocidad=vel;
@@ -15,20 +22,33 @@ public class Particula{
         radio=r;
     }
 
+    /** Obtener la posicion
+     *
+     * @return La posicion de la particula
+     */
     public Punto posicion(){
         return posicion;
     }
 
+    /** Obtener radio
+     *
+     * @return El radio de la particula
+     */
     public double radio(){
         return radio;
     }
 
+    /**  Calcular la fuerza	de atracción que ejerce la	partícula part sobre la	receptora
+     *
+     * @param p La otra particula
+     * @return Vector fuerza atraccion
+     */
     public Vector fuerzaDesde(Particula p){
 
         Vector direccion = new Vector(this.posicion, p.posicion());
 
-        // Cálculo de la distancia entre las posiciones
-        double distancia = direccion.modulo(); // ||r2 - r1||
+        // Cálculo de la distancia
+        double distancia = direccion.modulo();
 
         // Cálculo del vector unitario
         Vector u12 = direccion.direccion();
@@ -40,18 +60,25 @@ public class Particula{
         return u12.escalar(fuerza);
     }
 
+    /** Aplicar	una	fuerza a la partícula durante un tiempo
+     *
+     * @param vz Fuerza que se le aplica a la particula
+     * @param dt El intervalo de tiempo durante el cual se aplica la fuerza
+     */
     public void mueve(Vector vz, double dt) {
-
+        // Cálculo de la aceleración a = F / m
         Vector aceleracion = vz.escalar(1 / masa);
-
-
+        // Nueva velocidad de la particula
         this.velocidad = this.velocidad.sum(aceleracion.escalar(dt));
-
-
+        // Nueva posicion de la particula
         this.posicion = this.posicion.sum(velocidad.escalar(dt));
     }
 
 
+    /** Representacion de los atributos de la particula
+     *
+     * @return Cadena con los atributos de la particula
+     */
     @Override
     public String toString() {
         return "Particula{" +
